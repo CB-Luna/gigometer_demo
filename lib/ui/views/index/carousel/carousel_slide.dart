ec0addf81c84b51a920873363fec98a99651d6be
lynatical <carolinaroblero8@gmail.com>
2023-05-08 17:15:30 -0700

    Se actualiza llamado a cont. de Strapi

--- lib/ui/views/index/carousel/carousel_slide.dart
+++ lib/ui/views/index/carousel/carousel_slide.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
+import '../../../../services/graphql_config.dart';
 import '../../../widgets/primary_button.dart';
 
 class CarouselSlide extends StatelessWidget {
@@ -15,7 +16,7 @@ class CarouselSlide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -29,8 +30,17 @@ class CarouselSlide extends StatelessWidget {
               letterSpacing: -0.25,
             ),
           ),
+          if (promo['Media'] != null)
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxHeight: 140),
+              child: Image.network(
+                setPath(promo['Media']['data']['attributes']['url']),
+              ),
+            ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            padding: promo['Media'] != null
+                ? const EdgeInsets.only(bottom: 5.0)
+                : const EdgeInsets.symmetric(vertical: 10.0),
             child: Text(
               promo['Paragraph'],
               textAlign: TextAlign.center,
@@ -43,7 +53,7 @@ class CarouselSlide extends StatelessWidget {
           ),
           for (var button in promo['Buttons'])
             SizedBox(
-              width: 150,
+              width: 125,
               child: FittedBox(
                 child: PrimaryButton(
                   text: button['Text'],
